29dc140332f240b377c1cf942e8781bdc6c94625
Gino Maisto <luigi.maisto@gmail.com>
2016-04-05 21:31:19 +0200

    Minor correction: increased keepalive interval

--- app/application.cpp
+++ app/application.cpp
@@ -3,8 +3,8 @@
 
 // If you want, you can define WiFi settings globally in Eclipse Environment Variables
 #ifndef WIFI_SSID
-	#define WIFI_SSID "XXXXXXXXXX" // Put you SSID and Password here
-	#define WIFI_PWD "YYYYYYYYYYY"
+	#define WIFI_SSID "xxxxxxxxxxx" // Put you SSID and Password here
+	#define WIFI_PWD "yyyyyyyyyyy"
 #endif
 
 // ... and/or MQTT username and password
@@ -15,20 +15,23 @@
 
 // ... and/or MQTT host and port
 #ifndef MQTT_HOST
-	#define MQTT_HOST "192.168.2.37"
+	#define MQTT_HOST "192.168.2.16"
 	#define MQTT_PORT 1883
 #endif
 
 
 #define FGDATA_ENDPOINT "fgdata"
 #define LIGHT1_ENDPOINT "light1"
+#define CHECK_ENDPOINT "status/check"
 #define MIN_MSG_LENGTH 10
 #define RELAY_PIN 4 // GPIO4
+#define STATION_NAME "st1"
 
 
 // Forward declarations
 void startMqttClient();
 void onMessageReceived(String topic, String message);
+
 
 Timer procTimer;
 bool light_status = false;
@@ -51,20 +54,41 @@ void checkMQTTDisconnect(TcpClient& client, bool flag){
 	procTimer.initializeMs(2 * 1000, startMqttClient).start(); // every 2 seconds
 }
 
+
 // Publish our message
 void publishMessage(bool ls)
 {
+
+  bool result = false;
 
 	if (mqtt.getConnectionState() != eTCS_Connected)
 		startMqttClient(); // Auto reconnect
 
   if(ls == true)
-    mqtt.publish(LIGHT1_ENDPOINT, "ON"); // or publishWithQoS
+    result = mqtt.publish(LIGHT1_ENDPOINT, "ON"); // or publishWithQoS
   else
-    mqtt.publish(LIGHT1_ENDPOINT, "OFF"); // or publishWithQoS
+    result = mqtt.publish(LIGHT1_ENDPOINT, "OFF"); // or publishWithQoS
 	//Serial.println("Let's publish message now!");
 	//mqtt.publish("main/frameworks/sming", "Hello friends, from Internet of things :)"); // or publishWithQoS
+
+  if (result == false) {
+    Serial.println("Error on publishing message");
+    // startMqttClient(); // Auto reconnect
+  }
 }
+
+
+
+// // Publish our message
+// void publishMessage()
+// {
+// 	if (mqtt.getConnectionState() != eTCS_Connected)
+// 		startMqttClient(); // Auto reconnect
+
+// 	Serial.println("Let's publish message now!");
+// 	mqtt.publish("main/frameworks/sming", "Hello friends, from Internet of things :)"); // or publishWithQoS
+// }
+
 
 // Callback for messages, arrived from MQTT server
 void onMessageReceived(String topic, String message)
@@ -107,6 +131,16 @@ void onMessageReceived(String topic, String message)
   }
 }
 
+
+
+// // Callback for messages, arrived from MQTT server
+// void onMessageReceived(String topic, String message)
+// {
+// 	Serial.print(topic);
+// 	Serial.print(":\r\n\t"); // Pretify alignment for printing
+// 	Serial.println(message);
+// }
+
 // Run MQTT client
 void startMqttClient()
 {
@@ -114,16 +148,17 @@ void startMqttClient()
 	if(!mqtt.setWill("last/will","The connection from this device is lost:(", 1, true)) {
 		debugf("Unable to set the last will and testament. Most probably there is not enough memory on the device.");
 	}
-	mqtt.connect("esp8266smig", MQTT_USERNAME, MQTT_PWD);
+  mqtt.setKeepAlive(180);
+	mqtt.connect("esp8266v11");
 	// Assign a disconnect callback function
 	mqtt.setCompleteDelegate(checkMQTTDisconnect);
-	mqtt.subscribe("fgdata");
+  mqtt.subscribe("fgdata");
 }
 
 // Will be called when WiFi station was connected to AP
 void connectOk()
 {
-  //	Serial.println("I'm CONNECTED");
+	Serial.println("I'm CONNECTED");
 
 	// Run MQTT client
 	startMqttClient();
@@ -143,17 +178,14 @@ void connectFail()
 void init()
 {
 	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
-	Serial.systemDebugOutput(false); // Debug output to serial
-
-
+	Serial.systemDebugOutput(true); // Debug output to serial
 
 	WifiStation.config(WIFI_SSID, WIFI_PWD);
 	WifiStation.enable(true);
 	WifiAccessPoint.enable(false);
 
-
   pinMode(RELAY_PIN, OUTPUT);
 
 	// Run our method when station was connected to AP (or not connected)
-	WifiStation.waitConnection(connectOk, 20, connectFail); // We recommend 20+ seconds for connection timeout at start
+	WifiStation.waitConnection(connectOk, 30, connectFail); // We recommend 20+ seconds for connection timeout at start
 }
